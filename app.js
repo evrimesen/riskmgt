fa06f8397786e4e2e4e31e07082320284d242635
evrimesen <75859773+evrimesen@users.noreply.github.com>
2020-12-12 11:29:32 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,16 +6,37 @@ const submitButton = document.querySelector("#btnSubmit")
 const finImpact = document.querySelector("#financialImpact");
 const repImpact = document.querySelector("#reputationalImpact");
 const riskTitle = document.querySelector("#riskTitle");
+const finImpactNum = document.querySelector("#finImpactNum"); 
 
-// let resultFinancialImpact = false;
+let finImpactState = false;
+let repImpactState = false;
+let numImpact = 0;
+
 let impactLang = [];
 let impactResult = "";
 
-// finImpact.addEventListener('change', function(e){
-//     resultFinancialImpact = finImpact.checked;
-//     if(resultFinancialImpact = true) {
-//         impactLang.push("financial");
-//     }})
+
+finImpact.addEventListener('change',function(e){
+    finImpactState = finImpact.checked;
+    if(finImpact.checked) {
+        finImpactNum.style.display = "block";
+        numImpact +=1}
+    else{
+        finImpactNum.style.display = "none";
+        finImpactAmt.value = "";
+        numImpact -=1
+        }
+})
+
+repImpact.addEventListener('change',function(e){
+    repImpactState = finImpact.checked;
+    if(repImpact.checked) {
+        numImpact +=1
+    } else {
+        numImpact -=1;
+    }
+})
+
 
 
 form.addEventListener('submit',async function(e){
@@ -26,11 +47,19 @@ form.addEventListener('submit',async function(e){
     let riskCause = form.elements.riskCause.value;
     let riskProb = form.elements.prob.value;
     let title = form.elements.riskTitle.value;
-    if(finImpact.checked) {impactLang.push("financial")};
-    if(repImpact.checked) {impactLang.push("reputational")};
-    impactLang.forEach(element => impactResult += `${element},`);
+    let finImpactAmt = form.elements.finImpactAmt.value;
+    if(finImpact.checked) {impactLang.push(`financial impact of $${finImpactAmt}.00`)};
+    if(repImpact.checked) {impactLang.push("reputational impact")};
+    if(impactLang.length <= 0) {impactResult = "no impact"}
+    else {
+        impactLang.forEach(function(item){
+            let i = impactLang.length-1;
+            if (item != impactLang[i]){
+                impactResult += `${item} and `
+            } else {impactResult += `${item}`}
+        })};
     riskTitle.append(`Risk Title: ${title}`);
-    riskResult.append(`The risk of ${riskEvent} due to ${riskCause} with a probability of occurrence of ${riskProb}%, may result in ${riskConseq} with ${impactLang} impact`);
+    riskResult.append(`The risk of ${riskEvent} due to ${riskCause} with a probability of occurrence of ${riskProb}%, may result in ${riskConseq} with ${impactResult}.`);
     resultContainer.style.display = "block";
     submitButton.disabled=true;
     })
@@ -44,10 +73,14 @@ clearButton.addEventListener('click',async function(e){
      form.elements.riskConseq.value = "";
      form.elements.riskCause.value = "";
      form.elements.riskTitle.value = "";
+     form.elements.finImpactAmt.value = "";
+     finImpactNum.style.display = "none";
      form.elements.prob.value = 50;
      finImpact.checked = false;
      repImpact.checked = false;
      impactLang = [];
      impactResult = "";
      submitButton.disabled=false;
-    })
+    });
+
+
